0d1e87422dcd64474f005caa935509095185d4c9
Ankita <ankita0404.bgml@gmail.com>
2025-08-05 21:55:43 +0530

    Γ£¿ Daily update - Added latest DSA questions

---
+++ Recursion/mergeSort.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr, int st, int mid, int end){
+    vector<int> temp;
+    int i = st, j = mid+1;
+    while( i <= mid && j <= end){
+        // if(arr[i] >= arr[j]){    for decending order
+        if(arr[i] <= arr[j]){       //for acending order
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else{
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while( i <= mid){
+      temp.push_back(arr[i]);
+        i++;  
+    }
+    while( j <= end){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for(int idx = 0; idx < temp.size(); idx++){
+        arr[idx + st] = temp[idx];
+    }
+}
+void mergeSort(vector<int> &arr, int st, int end){
+    if(st < end){
+        int mid = st + (end - st)/2;
+
+        mergeSort(arr, st, mid); //left half
+        mergeSort(arr, mid + 1, end); //right half
+
+        merge(arr, st, mid, end);
+    }
+}
+int main(){
+    vector<int> arr = {12, 31, 17, 8, 32};
+    //function call
+
+    mergeSort(arr, 0, arr.size()-1);
+    for(int val: arr){
+        cout<< val <<" ";
+    }
+    cout<<endl;
+    return 0;
+}
